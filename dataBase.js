627b8aa5ab9df8028d931363007d72c636f0e096
madevgit <moukadimalassani@gmail.com>
2021-08-25 10:04:56 +0100

    test commit

--- dataBase.js
+++ dataBase.js
@@ -1,12 +1,19 @@
-const mongoose= require('mongoose')
-const URI= process.env.LOCAL_URI
-module.exports= async(callback)=>{
-    try{
-        await mongoose.connect(URI,{useNewUrlParser:true,useUnifiedTopology:true})
-        console.log('db connnection valide')
-        await callback()
-    }catch(e){
-        console.log(e)
-        throw e
-    }
-}
+const mongoose = require("mongoose");
+module.exports = async (callback) => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://DimVision:NewLifeVision99@cluster0.mp1sh.mongodb.net/qoseditor",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      }
+    );
+    console.log("db connnection established");
+    await callback();
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
